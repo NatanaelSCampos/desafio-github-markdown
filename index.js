1b5757fcd9de09683088a0dc7cf493c0411abb27
Natanael S Campos <59377656+NatanaelSCampos@users.noreply.github.com>
2024-10-05 16:56:19 -0300

    adicionando campo para solicitar nome do jogador.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// Solicita o nome do jogador
+let nomeJogador = prompt("Digite seu nome:");
+
 // Gera um número aleatório entre 1 e 100
 let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 let tentativas = 0;
 let numeroUsuario = 0;
 
-// Função para solicitar um número até o usuário acertar
+// Função para solicitar um número até o jogador acertar
 while (numeroUsuario !== numeroAleatorio) {
-    numeroUsuario = parseInt(prompt("Adivinhe o número (entre 1 e 100):"));
+    numeroUsuario = parseInt(prompt(`${nomeJogador}, adivinhe o número (entre 1 e 100):`));
     tentativas++;
 
     if (numeroUsuario > numeroAleatorio) {
@@ -13,6 +16,6 @@ while (numeroUsuario !== numeroAleatorio) {
     } else if (numeroUsuario < numeroAleatorio) {
         console.log("O número é maior.");
     } else {
-        console.alert(`Parabéns! Você acertou o número ${numeroAleatorio} em ${tentativas} tentativas.`);
+        console.alert(`Parabéns, ${nomeJogador}! Você acertou o número ${numeroAleatorio} em ${tentativas} tentativas.`);
     }
 }
